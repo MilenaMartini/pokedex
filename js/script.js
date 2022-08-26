5007b9e4d6f223e52158ec2f5f1d1744cd0a5c4c
Milena <Milena@etec.com>
2022-08-26 15:26:50 -0300

    começo js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const pokemonName = document.querySelector('.pokemon__name');
+
+const fetchPokemon = async (pokemon) => {
+
+    const APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+    
+    const data = await APIResponse.json();
+    return data;
+}
+
+const renderPokemon = async (pokemon) => {
+
+    const data = await fetchPokemon(pokemon);
+
+    pokemonName.innerHTML = data.name;
+}
+
+renderPokemon('234')
