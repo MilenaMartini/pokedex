59e869d4c91afb2368836879d9571dbb56e80724
Milena <Milena@etec.com>
2022-08-26 15:44:08 -0300

    começo das imagens no js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 const pokemonName = document.querySelector('.pokemon__name');
+const pokemonNumber = document.querySelector('.pokemon__number');
+const pokemonImage = document.querySelector('.pokemon__image');
+
+
+
 
 const fetchPokemon = async (pokemon) => {
 
@@ -13,6 +18,10 @@ const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
 
     pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonNumber.scr = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+
+
 }
 
 renderPokemon('5')
